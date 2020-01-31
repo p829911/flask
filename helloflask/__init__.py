b5cb7ebd4fce2a70960309eab4772ff9a2311fd8
p829911 <p829911@gmail.com>
2020-01-31 19:47:01 +0900

    first commit

---
+++ helloflask/__init__.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask import g
+from flask import Response
+from flask import make_response
+
+app = Flask(__name__)
+app.debug = True
+
+# @app.before_request
+# def before_request():
+#     print('before_request!!!')
+#     g.str = '한글'
+
+@app.route('/test_wsgi')
+def wsgi_test():
+    def application(environ, start_response):
+        body = 'The request method was %s' % environ['REQUEST_METHOD']
+        headers = [
+            ('Content-Type', 'text/plain'),
+            ('Content-Length', str(len(body)))
+        ]
+        start_response('200 OK', headers)
+        return [body]
+    
+    return make_response(application)
+
+
+@app.route('/gg') 
+def helloworld2():
+    return "Hello Flask World!" + getattr(g, 'str', '111')
+
+
+@app.route('/res1')
+def res1():
+    custom_res = Response('Custom Response', 200, {'test': 'ttt'})
+    return make_response(custom_res)
+
+ 
+@app.route('/') 
+def helloworld():
+    return "Hello Flask World!"
