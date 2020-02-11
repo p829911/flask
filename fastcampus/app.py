059ea5cf70aff011e21bda1af9c30434b8cc1473
p829911 <p829911@gmail.com>
2020-02-11 11:29:13 +0900

    200211

---
+++ fastcampus/app.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask
+from flask import request
+from flask import redirect
+from flask import render_template
+from models import db
+
+app = Flask(__name__)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        # 회원 정보 생성
+        return redirect('/')
+
+
+@app.route('/')
+def hello():
+    return render_template('hello.html')
+
+
+if __name__ == '__main__':
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    dbfile = os.path.join(basedir, 'db.sqlite')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    db.app = app
+    db.create_all()
+
+    app.run(host='127.0.0.1', port=5000, debug=True)
